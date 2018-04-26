a836b4c0233c60a9f4ac3aebd10acce36d4d7b94
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2018-04-26 13:02:45 +0200

    fixed bugs

--- js/subpage.js
+++ js/subpage.js
@@ -3,12 +3,12 @@ let urlParams = new URLSearchParams(window.location.search);
 let id =urlParams.get('id');
 console.log('I want to get to article '+ id);
 
-fetch('http://wilmakorpinen.com/wp00/wp-json/wp/v2/music/'+id).then(e =>e.json).then(showSinglePost);
+fetch('http://wilmakorpinen.com/wp00/wp-json/wp/v2/films?_embed/'+id).then(e =>e.json).then(showSinglePost);
 
 function showSinglePost(aPost){
     console.log(aPost);
-    document.querySelector('#singleMusic h1').textContent = aPost.title.rendered;
+    document.querySelector('#singleMovie h1').textContent = aPost.title.rendered;
     
     // show carsection
-    document.querySelector('#singleMusic').classList.add('slideInMusic')
+    document.querySelector('#singleMovie').classList.add('slideInMovie')
 }
