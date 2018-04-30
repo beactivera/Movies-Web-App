d5e79e201c213be3f537e17a1d8f34196265fee1
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2018-04-30 12:09:20 +0200

    started 2nd iteration

--- js/subpage.js
+++ js/subpage.js
@@ -8,10 +8,10 @@ fetch('http://wilmakorpinen.com/wp00/wp-json/wp/v2/films/'+id).then(e => e.json(
 function showSinglePost(aPost){
     console.log(aPost);
     document.querySelector('#singleMovie h1').textContent = aPost.title.rendered;
-//    document.querySelector("img").setAttribute("src", aPost._links._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url);
-    document.querySelector('#singleMovie h2').textContent = 'genre: ' + aPost.acf.genre;
-    document.querySelector('#singleMovie span').textContent = 'price: ' + aPost.acf.price;
-    document.querySelector('#singleMovie .date').textContent = 'data: ' + aPost.acf.data;
-    document.querySelector('#singleMovie .location').textContent = 'location: '+ aPost.acf.location;
-    document.querySelector('#singleMovie .descript').innerHTML = 'description: ' + aPost.content.rendered;
+// document.querySelector("img").setAttribute("src", aPost._links._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url);
+    document.querySelector('#singleMovie h2').textContent = aPost.acf.genre;
+    document.querySelector('#singleMovie span').textContent =aPost.acf.price;
+    document.querySelector('#singleMovie .date').textContent = aPost.acf.data;
+    document.querySelector('#singleMovie .location').textContent = aPost.acf.location;
+    document.querySelector('#singleMovie .descript').innerHTML = aPost.content.rendered;
 }
